3687513aab15e45769a7c7046b5669a3b2d44008
Lavrenov Egor <egor.uriewitch@gmail.com>
2021-03-16 18:47:11 +0200

    Router: добавил страницу

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,34 @@
+<script>
+export default {
+  data() {
+    return {
+      nav: [
+        {
+          name: 'home',
+          text: 'Home',
+        },
+        {
+          name: 'about',
+          text: 'About',
+        },
+      ],
+    }
+  },
+}
+</script>
+
 <template>
   <div id="app">
+    <div id="nav">
+      <router-link
+        class="link"
+        v-for="(link, i) in nav"
+        :key="i"
+        :to="{ name: link.name }"
+      >
+        {{ link.text }}
+      </router-link>
+    </div>
     <router-view />
   </div>
 </template>
@@ -14,5 +43,27 @@
   background-color: #f6f6f4;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+
+#nav {
+  display: flex;
+  justify-content: center;
+  padding: 12px 8px;
+  color: #fff;
+  background-color: rgb(44, 41, 42);
+
+  .link {
+    display: block;
+    font-size: 1.125rem;
+    font-weight: 500;
+    color: #fff;
+    text-decoration: none;
+    &:not(:last-of-type) {
+      margin-right: 1.25em;
+    }
+    &.router-link-exact-active {
+      text-decoration: underline;
+    }
+  }
 }
 </style>
---
+++ src/views/About.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <button
+      @click="
+        $router.push({
+          name: 'home',
+          query: {
+            sort: 'orders',
+            dir: 'asc',
+          },
+        })
+      "
+    >
+      Перейти на страницу таблицы используя программную навигацию
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+}
+</script>
+
+<style lang="scss" scoped>
+.button {
+  padding: 1em;
+  background-color: transparent;
+  border: 1px solid #222;
+}
+
+.container {
+  padding: 20px;
+  overflow-x: auto;
+}
+</style>
